cb5ebb9a6916d66fbc5777b61d3412268ab86727
Chenxu Robin Mao <robin.mao@mail.utoronto.ca>
2024-07-05 13:27:19 -0400

    set up python environment in matlab

---
+++ MountainCar.m
@@ -0,0 +1,7 @@
+%% This RL project will focus on the following github repo, https://github.com/openai/gym/blob/master/gym/envs/classic_control/mountain_car.py
+
+%% Update Python env here
+% pyenv('Version', '{INSERT PATH HERE}')
+
+%% Make environment
+env = py.gym.make('MountainCar-v0');
